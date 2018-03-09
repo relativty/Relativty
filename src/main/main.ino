2f25923b0c2d369918d1fff59dcc89ef395353d0
jwdinius <josephwdinius@gmail.com>
2018-03-08 20:15:32 -0800

    wip: adding inertial tracking algorithm from LaValle text

--- src/main/main.ino
+++ src/main/main.ino
@@ -22,6 +22,7 @@ MPU6050 mpu;
 Relativ relativ; 
 
 #define INTERRUPT_PIN 2
+#define FRAME_RATE 50
 
 // IMU status and control:
 bool dmpReady = false;  // true if DMP init was successful
@@ -33,6 +34,13 @@ uint8_t fifoBuffer[64];
 
 
 Quaternion q;           // [w, x, y, z]
+Quaternion qf;          // filtered quaternion
+
+VectorInt16 accel;         // [x, y, z]            accel sensor measurements
+VectorInt16 omega;         // [wx, wy, wz]         gyro sensor measurements
+int last_update;
+int cycle_time;
+long last_cycle;
 
 volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
 void dmpDataReady() {
@@ -67,7 +75,7 @@ void setup() {
     // ==================================
     // supply your own gyro offsets here:
     // ==================================
-
+    // follow procedure here to calibrate offsets https://github.com/kkpoon/CalibrateMPU6050
     mpu.setXGyroOffset(220);
     mpu.setYGyroOffset(76);
     mpu.setZGyroOffset(-85);
@@ -127,10 +135,43 @@ void loop() {
 * like Arduino Mega, Arduino Uno, etc. (slower)
 */
 
-        relativ.updateOrientationNative(q.x, q.y, q.z, q.w, 4); 
+        // do complementary filtering here
+        mpu.dmpGetAccel(&accel, fifoBuffer);
+
+        VectorFloat16 accf(float(accel.x), float(accel.y), float(accel.z));
+
+        // find "up" direction
+        VectorFloat16 up = accf.rotate(q);
+        
+        // tilt angle is the angle made between world-y and the "up"
+        float phi = acos( up.y / up.magnitude() );
+
+        // drift due to tilt
+        Quaternion d_tilt( cos(phi/2.f), up.normalize().z * sin(phi/2.f), 0.f, -up.normalize().x * sin(phi/2.f) );
+
+        // TO DO: if magnetometer readings are available, yaw drift can be accounted for
+        // int16_t ax_tmp, ay_tmp, az_tmp, gx_tmp, gy_tmp, gz_tmp, mx, my, mz;
+        // mpu.getMotion9(ax_tmp, ay_tmp, az_tmp, gx_tmp, gy_tmp, gz_tmp, mx, my, mz);
+        // VectorFloat16 magn(float(mx), float(my), float(mz));
+        // VectorFloat16 north = magn.rotate(q);
+        // float beta = acos( -north.z / north.magnitude() );
+        // 
+
+        relativ.updateOrientationNative(q.x, q.y, q.z, q.w, 4);
 
         //If your microcontroller is non-native usb, change the above line to:
         //relativ.updateOrientation(q.x, q.y, q.z, q.w, 4); 
     }
+}
+
+// ensure regular clock-rate
+void hold_time() {
+    long time = millis();
+    while ( (time - last_cycle) < FRAME_RATE ){
+        delay(1);
+        time = millis();
+    }
+    cycle_time = time - last_cycle;
+    last_cycle = time;
 }
 
