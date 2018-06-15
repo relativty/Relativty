c89f65f659498520feca01d917de6ab724a7587d
Joe Dinius, PhD <josephwdinius@gmail.com>
2018-06-15 14:13:31 -0700

    wip: [Feature Request] inertial tracking using the onboard imu #21 (#23)
    * wip: adding inertial tracking algorithm from LaValle text
    
    * wip: added drift correction logic for test
    
    * wip: added drift correction logic for test

--- src/main/main.ino
+++ src/main/main.ino
@@ -22,6 +22,9 @@ MPU6050 mpu;
 Relativ relativ; 
 
 #define INTERRUPT_PIN 2
+// set these to correct for drift - try small values ~0.000001
+#define ALPHA 0.f // correction for drift - tilt channel
+#define BETA  0.f // correction for drift - yaw channel
 
 // IMU status and control:
 bool dmpReady = false;  // true if DMP init was successful
@@ -30,7 +33,6 @@ uint8_t devStatus;      // 0 = success, !0 = error
 uint16_t packetSize;    
 uint16_t fifoCount;     
 uint8_t fifoBuffer[64]; 
-
 
 Quaternion q;           // [w, x, y, z]
 
@@ -67,7 +69,7 @@ void setup() {
     // ==================================
     // supply your own gyro offsets here:
     // ==================================
-
+    // follow procedure here to calibrate offsets https://github.com/kkpoon/CalibrateMPU6050
     mpu.setXGyroOffset(220);
     mpu.setYGyroOffset(76);
     mpu.setZGyroOffset(-85);
@@ -90,7 +92,6 @@ void setup() {
         // ERROR!
     }
 }
-
 
 void loop() {
     // Do nothing if DMP doesn't initialize correctly
@@ -127,7 +128,39 @@ void loop() {
 * like Arduino Mega, Arduino Uno, etc. (slower)
 */
 
-        relativ.updateOrientationNative(q.x, q.y, q.z, q.w, 4); 
+        // find direction of gravity (down in world frame)
+        VectorFloat16 down;
+        mpu.dmpGetGravity(&down, &q);
+        
+        // tilt angle is the angle made between world-y and the "up"-vector (just negative of "down")
+        float phi = acos( -down.y / down.magnitude() );
+
+        // drift due to tilt
+        Quaternion d_tilt( cos(phi/2.f), -down.normalize().z * sin(phi/2.f), 0.f, down.normalize().x * sin(phi/2.f) );
+
+        // TO DO: if magnetometer readings are available, yaw drift can be accounted for
+        // int16_t ax_tmp, ay_tmp, az_tmp, gx_tmp, gy_tmp, gz_tmp, mx, my, mz;
+        // mpu.getMotion9(ax_tmp, ay_tmp, az_tmp, gx_tmp, gy_tmp, gz_tmp, mx, my, mz);
+        // VectorFloat16 magn(float(mx), float(my), float(mz));
+        // VectorFloat16 north = magn.rotate(q);
+        // float beta = acos( -north.z / north.magnitude() );
+        // Quaternion d_yaw( cos(beta/2.f), 0.f, sin(beta/2.f), 0.f );
+        Quaternion d_yaw; // just default to identity if no magnetometer is available.
+
+        // COMPLEMENTARY FILTER (YAW CORRECTION * TILT CORRECTION * QUATERNION FROM ABOVE)
+        double tilt_correct = -ALPHA * 2.f * acos(d_tilt.w);
+        double yaw_correct  = -BETA  * 2.f * acos(d_yaw.w);
+        Quaternion tilt_correction( cos(tilt_correct/2.f), -down.normalize() * sin(tilt_correct/2.f), 0.f, down.normalize().x * sin(tilt_correct/2.f) );
+        Quaternion yaw_correction( cos(yaw_correct/2.f), 0.f, sin(yaw_correct/2.f), 0.f);
+
+        // qc = yc * tc * q
+        Quaternion qc(tilt_correction.w, tilt_correction.x, tilt_correction.y, tilt_correction.z);
+        qc.getProduct(yaw_correction);
+        qc.getProduct(tilt_correction);
+        qc.getProduct(q);
+
+        // report result
+        relativ.updateOrientationNative(qc.x, qc.y, qc.z, qc.w, 4);
 
         //If your microcontroller is non-native usb, change the above line to:
         //relativ.updateOrientation(q.x, q.y, q.z, q.w, 4); 
