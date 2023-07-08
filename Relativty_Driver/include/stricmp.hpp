a58a274dbdd47432d7c00bb6b857544ddadff2f8
Gabor Nemeth <etgabo@gmail.com>
2023-07-08 11:48:17 +0100

    fixed windows compile errors

--- Relativty_Driver/include/stricmp.hpp
+++ Relativty_Driver/include/stricmp.hpp
@@ -1,8 +1,12 @@
-#ifndef MSVC
+//https://botsikas.blogspot.com/2011/12/strcasecmp-identifier-not-found-when.html
 
-#include <strings.h>
+#if defined(_WIN32) || defined(_WIN64)
+
+#define snprintf _snprintf
+#define vsnprintf _vsnprintf
+#define strcasecmp _stricmp
+#define strncasecmp _strnicmp
 #define imp_stricmp strcasecmp
-
 #else
 
 #define imp_stricmp _stricmp
